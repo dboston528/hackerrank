3c376ebc4db6160979ce28bc68e98a17573047df
DeAndre Boston <dboston3@yahoo.com>
2019-09-17 22:58:47 -0500

    Update countingvalleys.js

--- countingvalleys.js
+++ countingvalleys.js
@@ -1,1 +1,24 @@
 
+function countingValleys(stepsCount, stepsString) {
+  let mountainCount = 0;
+  let valleyCount = 0;
+  let currentLevel = 0;
+  let previousLevel = 0;
+  
+  for (let i = 0; i < stepsString.length) {
+    previousLevel  = currentLevel;
+    if (stepsString[i] === 'U') {
+        currentLevel +=1;
+        } else {
+          currentLevel -= 1;
+        }
+    
+    if (previousLevel === 0 && currentLevel <0) {
+      vallwyCount += 1;
+    }
+  }
+  
+  return valleyCount;
+}
+
+console.log("Hello World");
